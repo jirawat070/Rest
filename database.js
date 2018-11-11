1743c4b484cb95e7be004c7b7a1beab6696a5a39
Jirawat070 <mongkonrat.nsw@gmail.com>
2018-11-12 06:20:05 +0700

    finish

--- database.js
+++ database.js
@@ -57,6 +57,11 @@ function insertProduct(req, res) {
         })
         .catch(function (error) {
             console.log('ERROR:', error)
+            res.status(200)
+            .json({
+                status: 'Failed',
+                message: 'Failed to Inserted one product'
+            });
         })
 }
 
@@ -72,6 +77,11 @@ function updateProduct(req, res) {
         })
         .catch(function (error) {
             console.log('ERROR:', error)
+            res.status(200)
+                .json({
+                    status: 'Failed',
+                    message: 'Failed to Update one product'
+                });
         })
 }
 
@@ -97,6 +107,249 @@ function deleteProduct(req, res) {
         })
 }
 
+//Purchase_item
+function getPurchase_item(req, res) {
+    db.any('select * from purchase_items')
+        .then(function (data) {
+            res.status(200)
+                .json({
+                    status: 'success',
+                    data: data,
+                    message: 'Retrieved ALL Purchase_item'
+                });
+        })
+        .catch(function (error) {
+            console.log('ERROR:', error)
+            console.log('ERROR:', error);
+            res.status(500)
+                .json({
+                    status: 'failed',
+                    data: data,
+                    message: 'Failed get purchase item' 
+                });
+        })
+}
+function getPurchase_itemByID(req, res) {
+    db.any('select * from purchase_items where id =' + req.params.id)
+        .then(function (data) {
+            res.status(200)
+                .json({
+                    status: 'success',
+                    data: data,
+                    message: 'Retrieved Purchase_items id:' + req.params.id
+                });
+        })
+        .catch(function (error) {
+            console.log('ERROR:', error)
+               res.status(500)
+                .json({
+                    status: 'failed',
+                    data: data,
+                    message: 'Failed to get purchase by id' + req.params.id
+                });
+        })
+}
+
+function insertPurchase_item(req, res) {
+    db.any('insert into purchase_items(id, purchase_id, product_id,price,quantity)' +
+        'values(${id}, ${purchase_id}, ${product_id}, ${price}, ${quantity})',
+        req.body)
+        .then(function (data) {
+            res.status(200)
+                .json({
+                    status: 'success',
+                    message: 'Inserted one Purchase_item'
+                });
+        })
+        .catch(function (error) {
+            console.log('ERROR:', error)
+        })
+
+    
+}
+function updatePurchase_item(req, res) {
+    db.any('update purchase_items set purchase_id=${purchase_id},product_id=${product_id},price=${price},quantity=${quantity} where id =' + req.params.id,
+        req.body)
+        .then(function (data) {
+            res.status(200).json({
+                status: 'success',
+                data: data,
+                message: 'Update Purchase_item id=' + req.params.id
+            });
+        })
+        .catch(function (error) {
+            console.log('ERROR:', error)
+        })
+}
+
+function DeletePurchase_item(req, res) {
+    db.any('delete from purchase_items where id=' + req.params.id)
+        .then(function (data) {
+            res.status(200)
+                .json({
+                    status: 'success',
+                    message: 'Delete id=' + req.params.id
+                })
+        })
+        .catch(function (error) {
+            console.log('ERROR:', error)
+        })
+        
+}
+
+//Purchase
+function getPurchase(req, res) {
+    db.any('select * from purchases')
+        .then(function (data) {
+            res.status(200)
+                .json({
+                    status: 'success',
+                    data: data,
+                    message: 'Retrieved ALL Purchase'
+                });
+        })
+        .catch(function (error) {
+            console.log('ERROR:', error)
+        })
+}
+function getPurchaseByID(req, res) {
+    db.any('select * from purchases where purchase_id =' + req.params.id)
+        .then(function (data) {
+            res.status(200)
+                .json({
+                    status: 'success',
+                    data: data,
+                    message: 'Retrieved Purchase id:' + req.params.id
+                });
+        })
+        .catch(function (error) {
+            console.log('ERROR:', error)
+        })
+}
+function insertPurchase(req, res) {
+    db.any('insert into purchases(purchase_id,created_at,name,address,state,zipcode,user_id)' +
+        'values(${purchase_id}, ${created_at}, ${name}, ${address}, ${status}, ${state},${zipcode},${user_id})',
+        req.body)
+        .then(function (data) {
+            res.status(200)
+                .json({
+                    status: 'success',
+                    message: 'Inserted one Purchase'
+                });
+        })
+        .catch(function (error) {
+            console.log('ERROR:', error)
+        })
+
+}
+function updatePurchase(req, res) {
+    db.any('update purchases set created_at=${created_at},name=${name},address=${address},state=${state},zipcode=${zipcode},user_id=${user_id} where purchase_id =' + req.params.id,
+        req.body)
+        .then(function (data) {
+            res.status(200).json({
+                status: 'success',
+                data: data,
+                message: 'Update Purchase id=' + req.params.id
+            });
+        })
+        .catch(function (error) {
+            console.log('ERROR:', error)
+        })
+}
+function DeletePurchase(req, res) {
+    db.any('delete from purchases where purchase_id=' + req.params.id)
+        .then(function (data) {
+            res.status(200)
+                .json({
+                    status: 'success',
+                    message: 'Delete id=' + req.params.id
+                })
+        })
+        .catch(function (error) {
+            console.log('ERROR:', error)
+        })
+}
+
+
+
+//user
+function getUser(req, res) {
+    db.any('select * from users')
+        .then(function (data) {
+            res.status(200)
+                .json({
+                    status: 'success',
+                    data: data,
+                    message: 'Retrieved ALL User'
+                });
+        })
+        .catch(function (error) {
+            console.log('ERROR:', error)
+        })
+}
+function getUserByID(req, res) {
+    db.any('select * from users where user_id =' + req.params.id)
+        .then(function (data) {
+            res.status(200)
+                .json({
+                    status: 'success',
+                    data: data,
+                    message: 'Retrieved Purchase_items id:' + req.params.id
+                });
+        })
+        .catch(function (error) {
+            res.status(500)
+                .json({ status: "fail", message: "Mission Fail get back" })
+            console.log('ERROR:', error)
+        })
+}
+function insertUser(req, res) {
+    db.any('insert into users(user_id,email,password,details,created_at)' +
+        'values(${user_id}, ${email}, ${password}, ${details}, ${created_at}',
+        req.body)
+        .then(function (data) {
+            res.status(200)
+                .json({
+                    status: 'success',
+                    message: 'Inserted one purchase'
+                });
+        })
+        .catch(function (error) {
+            console.log('ERROR:', error)
+        })
+
+    
+}
+function DeleteUser(req, res) {
+    db.any('delete from users where user_id=' + req.params.id)
+        .then(function (data) {
+            res.status(200)
+                .json({
+                    status: 'success',
+                    message: 'Delete id=' + req.params.id
+                })
+        })
+        .catch(function (error) {
+            console.log('ERROR:', error)
+        })
+  
+}
+function updateUser(req, res) {
+    db.any('update users set email=${email},password=${password},details=${details},created_at=${created_at} where user_id =' + req.params.id,
+        req.body)
+        .then(function (data) {
+            res.status(200).json({
+                status: 'success',
+                data: data,
+                message: 'update purchase id=' + req.params.id
+            });
+        })
+        .catch(function (error) {
+            console.log('ERROR:', error)
+        })
+}
+
+
 
 //fn ที่อยากให้ข้างนอกเห็นเอาไปใช้
 module.exports = {
@@ -104,5 +357,20 @@ module.exports = {
     getProductByID,
     insertProduct,
     updateProduct,
-    deleteProduct
+    deleteProduct,
+    getPurchase_item,
+    getPurchase_itemByID,
+    insertPurchase_item,
+    updatePurchase_item,
+    DeletePurchase_item,
+    getPurchase,
+    insertPurchase,
+    updatePurchase,
+    DeletePurchase,
+    getPurchaseByID,
+    getUser,
+    getUserByID,
+    insertUser,
+    DeleteUser,
+    updateUser
 };
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 var db = require('./database');
+var cors = require('cors')
+app.use(cors())
 
 
 var bodyParser = require('body-parser');
@@ -25,11 +27,33 @@ app.get('/api/json'
         res.status(500).json(output);
     });
 
+//product
 app.get('/api/products/',db.getAllProducts);
 app.get('/api/products/:id', db.getProductByID);
 app.post('/api/products', db.insertProduct);
 app.put('/api/products/:id', db.updateProduct);
 app.delete('/api/products/:id', db.deleteProduct);
+
+//user
+app.get('/api/users', db.getUser);
+app.get('/api/user/:id', db.getUserByID);
+app.post('/api/user', db.insertUser);
+app.put('/api/user/:id', db.updateUser);
+app.delete('/api/user/:id', db.DeleteUser);
+
+//purchase
+app.get('/api/purchase', db.getPurchase);
+app.get('/api/purchase/:id', db.getPurchaseByID);
+app.post('/api/purchase', db.insertPurchase);
+app.put('/api/purchase/:id', db.updatePurchase);
+app.delete('/api/purchase/:id', db.DeletePurchase);
+
+//Purchase_item
+app.get('/api/purchase_item', db.getPurchase_item);
+app.get('/api/purchase_item/:id', db.getPurchase_itemByID);
+app.post('/api/purchase_item', db.insertPurchase_item);
+app.put('/api/purchase_item/:id', db.updatePurchase_item);
+app.delete('/api/purchase_item/:id', db.DeletePurchase_item);
 
 var port = process.env.PORT || 8080;
 app.listen(port, function () {
